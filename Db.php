fddb7c29e03d8764273b485d4d61352f7bd98bb1
Gabriele Tozzi <gabriele@tozzi.eu>
2014-07-10 00:30:34 +0200

    DB: started support for "magic" table classes.
    ***BREAKS BACKWARD COMPATIBILITY***
    Db class methods _run, _insert, _update, _foundRows,
    __buildInsUpdQuery, __buildParams and _buildLimit are now public
    and their name no longer starts with underscores. (Es: run, insert,
    etc...). Every call must be refactored.

--- Db.php
+++ Db.php
@@ -38,7 +38,7 @@ class Db {
 	* @param $query string: The query to be executed
 	* @param $params mixed: Array containing the parameters or single parameter
 	*/
-	protected function _run($query, $params=array()) {
+	public function run($query, $params=array()) {
 		if( ! is_array($params) )
 			$params = array($params);
 		foreach($params as & $p)
@@ -58,10 +58,10 @@ class Db {
 	*
 	* @see __buildInsUpdQuery
 	*/
-	protected function _insert($table, $params) {
+	public function insert($table, $params) {
 		list($q,$p) = $this->__buildInsUpdQuery('ins', $table, $params);
 
-		$this->_run($q, $p);
+		$this->run($q, $p);
 		return $this->_pdo->lastInsertId();
 	}
 
@@ -71,14 +71,14 @@ class Db {
 	* @see __buildInsUpdQuery
 	* @param $where array: where conditions
 	*/
-	protected function _update($table, $params, $where) {
-		list($s,$ps) = $this->__buildInsUpdQuery('upd', $table, $params);
-		list($w,$pw) = $this->__buildParams($where);
+	public function update($table, $params, $where) {
+		list($s,$ps) = $this->buildInsUpdQuery('upd', $table, $params);
+		list($w,$pw) = $this->buildParams($where);
 
 		$q = "$s WHERE $w";
 		$p = array_merge($ps, $pw);
 
-		$this->_run($q, $p);
+		$this->run($q, $p);
 	}
 
 	/**
@@ -86,10 +86,10 @@ class Db {
 	*
 	* @return int: Number of found rows
 	*/
-	protected function _foundRows() {
+	public function foundRows() {
 		$q = "SELECT FOUND_ROWS() AS `fr`";
 
-		$res = $this->_run($q)->fetch();
+		$res = $this->run($q)->fetch();
 		return $res['fr'] !== null ? (int)$res['fr'] : null;
 	}
 
@@ -99,10 +99,10 @@ class Db {
 	*
 	* @param $type string: The query type (ins, upd)
 	* @param $table string: The name of the table
-	* @see __buildParams
+	* @see buildParams
 	* @return array [query string, params]
 	*/
-	private function __buildInsUpdQuery($type, $table, $params) {
+	public function buildInsUpdQuery($type, $table, $params) {
 		switch( $type ) {
 		case 'ins':
 			$q = 'INSERT INTO';
@@ -111,10 +111,10 @@ class Db {
 			$q = 'UPDATE';
 			break;
 		default:
-			throw \Exception("Unknown type $type");
+			throw new \Exception("Unknown type $type");
 		}
 
-		list($cols, $p) = $this->__buildParams($params);
+		list($cols, $p) = $this->buildParams($params);
 		$q .= " `$table` SET $cols" ;
 		return array($q, $p);
 	}
@@ -127,9 +127,10 @@ class Db {
 	*                       of the column. If the data is an array, 2nd key is
 	*                       the custom placeholder to use. By example:
 	*                       'password' => [ '123456', 'SHA1(?)' ]
+	* @param $glue string: The string to join the arguments, usually ', ' or ' AND '
 	* @return array [query string, params]
 	*/
-	private function __buildParams($params) {
+	public function buildParams($params, $glue=', ') {
 		$cols = array();
 		$vals = array();
 		foreach( $params as $k => $v ) {
@@ -144,7 +145,7 @@ class Db {
 			}
 			$cols[] = $c;
 		}
-		return array(implode(', ', $cols), $vals);
+		return array(implode($glue, $cols), $vals);
 	}
 
 	/**
@@ -155,7 +156,7 @@ class Db {
 	* @param $skip integer: Number of records to skip
 	* @return string: the LIMIT statement
 	*/
-	protected function _buildLimit($limit=null, $skip=0) {
+	public function buildLimit($limit=null, $skip=0) {
 		if( $skip || $limit )
 			$lim = 'LIMIT ';
 		else
@@ -170,6 +171,158 @@ class Db {
 			$lim = (int)$limit;
 
 		return $lim;
+	}
+
+}
+
+/**
+* Represents a table on the database for easy automated access
+*/
+class Table {
+
+	/** Database table name, may be overridden is sub-class or passed by constructor */
+	protected $_name = null;
+	/** Database object instance, passed by constructor */
+	protected $_db = null;
+	/** Column definition cache, populated at runtime */
+	protected $_cols = array();
+	/** Primary key cache, populated at runtime */
+	protected $_pk = array();
+
+	/**
+	* Creates the table object
+	*
+	* @param $db object: The Db instance
+	* @param $name string: The table name, override the given one
+	*/
+	public function __construct($db, $name=null) {
+
+		// Assign and check parameters
+		$this->_db = $db;
+		if( $name )
+			$this->_name = $name;
+		if( ! $this->_db instanceof Db )
+			throw new \Exception('Db must be a valid dophp\Db instance');
+		if( ! $this->_name || gettype($this->_name) !== 'string' )
+			throw new \Exception('Unvalid table name');
+
+		// Read and cache table structure
+		$q = "
+			SELECT
+				`COLUMN_NAME`,
+				`COLUMN_DEFAULT`,
+				`IS_NULLABLE`,
+				`DATA_TYPE`,
+				`CHARACTER_MAXIMUM_LENGTH`,
+				`NUMERIC_PRECISION`,
+				`NUMERIC_SCALE`,
+				`COLUMN_KEY`,
+				`EXTRA`
+			FROM `information_schema`.`COLUMNS`
+			WHERE `TABLE_SCHEMA` = DATABASE() AND `TABLE_NAME` = ?
+			ORDER BY `ORDINAL_POSITION`
+		";
+		foreach( $this->_db->run($q, array($this->_name))->fetchAll() as $c ) {
+			$this->_cols[$c['COLUMN_NAME']] = $c;
+			if( $c['COLUMN_KEY'] == 'PRI' )
+				$this->_pk[] = $c['COLUMN_NAME'];
+		}
+
+	}
+
+	/**
+	* Gets a record by PK
+	*
+	* @param $pk mixed The primary key, array if composite (associative or numeric)
+	* @return The fetched row
+	*/
+	public function get($pk) {
+		// Check parameters
+		if( ! $this->_pk )
+			throw new \Exception('Table doesn\'t have a Primary Key');
+		if( ! is_array($pk) )
+			$pk = array($pk);
+		if( count($this->_pk) != count($pk) )
+			throw new \Exception('Number of columns in Primary Key doesn\'t match');
+
+		// Match arguments, replace numeric with associative array elements
+		foreach( $pk as $k => $v )
+			if( is_int($k) ) {
+				$pk[$this->_pk[$k]] = $v;
+				unset($pk[$k]);
+			} else
+				if( ! array_key_exists($k, $this->_pk) )
+					throw new \Exception("Unknown column name in PK: $k");
+		
+		// Build and run the query
+		$q = "
+			SELECT *
+			FROM `{$this->_name}`
+			WHERE ";
+		$p = array();
+		$first = true;
+		foreach( $pk as $k => $v ) {
+			if( $first )
+				$first = false;
+			else
+				$q ."\n\tAND ";
+			$q .= "`$k` = ?";
+			$p[] = $v;
+		}
+		return $this->_db->run($q,$p)->fetch();
+	}
+
+	/**
+	* Runs a select query
+	*
+	* @param $params array See dophp\Db::buildParams
+	* @param $cols array Names of the columns. null to select all. true to
+	*                    select only PKs.
+	* @param $limit mixed Numeric limit, boolean TRUE means 1. If array,
+	*                     must contain two elements: first element is $limit,
+	*                     second is $skip. If null, no limit.
+	*                     See dophp\Db::buildLimit
+	* @see dophp\Db::buildParams
+	* @see dophp\Db::buildLimit
+	* @return mixed The single fetched row or array(fetched rows, number of records found)
+	*/
+	public function select($params, $cols=null, $limit=null) {
+		$q = "SELECT\n\t";
+		if( ! $cols )
+			$q .= "\t*";
+		else {
+			if( $cols === true )
+				$cols = $this->_pk;
+			$first = true;
+			foreach( $cols as $c ) {
+				if( ! array_key_exists($c, $this->_cols) )
+					throw new \Exception("Unknown column name: $c");
+				if( $first )
+					$first = false;
+				else
+					$q .= ",\n\t";
+				$q .= "`$c`";
+			}
+		}
+		$q .= "\n";
+
+		$q .= "FROM `{$this->_name}`\n";
+
+		$q .= "WHERE ";
+		list($w, $p) = $this->_db->buildParams($params, ' AND ');
+		$q .= "$w\n";
+
+		if( is_array($limit) )
+			list($limit, $skip) = $limit;
+		else
+			$skip = null;
+		$q .= $this->_db->buildLimit($limit, $skip);
+		
+		$st = $this->_db->run($q, $p);
+		if( $limit === true || $limit === 1 )
+			return $st->fetch();
+		else
+			return array($st->fetchAll(), $this->_foundRows());
 	}
 
 }
