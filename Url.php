2a00509989c51b7cb3d4c620d9c138d55dab0b56
Gabriele Tozzi <gabriele@tozzi.eu>
2020-06-30 18:05:05 +0200

    PHP 7.4 deprecation fix

--- Url.php
+++ Url.php
@@ -163,7 +163,7 @@ class Url {
 		if( ! isset($url['scheme']) )
 			$url['scheme'] = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on' ? 'https' : 'http';
 		if( ! isset($url['host']) ) {
-			$url['host'] = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:$_SERVER['SERVER_NAME'];
+			$url['host'] = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : (isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:$_SERVER['SERVER_NAME']);
 			$url['host'] = explode(':', $url['host']);
 			$url['host'] = $url['host'][0];
 		}
